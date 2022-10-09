00b360237b3ee0d5ef363a2ece64b8ba1af19d0f
Mark Burggraf <markb@mantisbible.com>
2022-10-09 07:15:40 -0700

    0.0.2

---
+++ docs/assets/_...fallback_.4526073f.js
@@ -0,0 +1,1 @@
+import{S as a,i as r,s as i,B as l}from"./index.dbf26c8d.js";import{g as u}from"./index.89c9e2e1.js";function c(t,o,e){let s;return l(t,u,n=>e(0,s=n)),s("/"),[]}class m extends a{constructor(o){super(),r(this,o,c,null,i,{})}}export{m as default};
