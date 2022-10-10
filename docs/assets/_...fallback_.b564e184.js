52e4f1640d3ad621a5d43885f0e8c42a21e345fe
Mark Burggraf <markb@mantisbible.com>
2022-10-10 16:20:50 -0700

    removed badge code

---
+++ docs/assets/_...fallback_.b564e184.js
@@ -0,0 +1,1 @@
+import{S as a,i as r,s as i,B as l}from"./index.c4920b7e.js";import{g as u}from"./index.8ea8d20a.js";function c(t,o,e){let s;return l(t,u,n=>e(0,s=n)),s("/"),[]}class m extends a{constructor(o){super(),r(this,o,c,null,i,{})}}export{m as default};
