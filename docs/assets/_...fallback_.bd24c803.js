6da6f92f22af3b9a1c45f8374617fad6e5eca89e
Mark Burggraf <markb@mantisbible.com>
2022-10-11 06:52:58 -0700

    0.0.12

---
+++ docs/assets/_...fallback_.bd24c803.js
@@ -0,0 +1,1 @@
+import{S as a,i as r,s as i,B as l}from"./index.28fa8daa.js";import{g as u}from"./index.c59c99ee.js";function c(t,o,e){let s;return l(t,u,n=>e(0,s=n)),s("/"),[]}class m extends a{constructor(o){super(),r(this,o,c,null,i,{})}}export{m as default};
--- vite.config.ts
+++ vite.config.ts
@@ -12,47 +12,47 @@ const pwaManifest = {
   theme_color: '#000000',
   icons: [
     {
-      "src": "icons/icon-32.png",
+      "src": "assets/icons/icon-32.png",
       "sizes": "32x32",
       "type": "image/png"
     },
     {
-      "src": "icons/icon-64.png",
+      "src": "assets/icons/icon-64.png",
       "sizes": "64x64",
       "type": "image/png"
     },
     {
-      "src": "icons/icon-96.png",
+      "src": "assets/icons/icon-96.png",
       "sizes": "96x96",
       "type": "image/png"
     },
     {
-      "src": "icons/icon-128.png",
+      "src": "assets/icons/icon-128.png",
       "sizes": "128x128",
       "type": "image/png"
     },
     {
-      "src": "icons/icon-168.png",
+      "src": "assets/icons/icon-168.png",
       "sizes": "168x168",
       "type": "image/png"
     },
     {
-      "src": "icons/icon-192.png",
+      "src": "assets/icons/icon-192.png",
       "sizes": "192x192",
       "type": "image/png"
     },
     {
-      "src": "icons/icon-256.png",
+      "src": "assets/icons/icon-256.png",
       "sizes": "256x256",
       "type": "image/png"
     },
     {
-      "src": "icons/icon-384.png",
+      "src": "assets/icons/icon-384.png",
       "sizes": "384x384",
       "type": "image/png"
     },
     {
-      "src": "icons/icon-512.png",
+      "src": "assets/icons/icon-512.png",
       "sizes": "512x512",
       "type": "image/png"
     }]
