a418e9f4c239615403bcba7aa1c8c227ab6d9bb0
Mark Burggraf <markb@mantisbible.com>
2022-10-19 08:55:11 -0700

    better date display on calendar page

--- src/routes/calendar.svelte
+++ src/routes/calendar.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import IonPage from "$ionic/svelte/components/IonPage.svelte";
-	import { currentUser } from '$services/user.store';
-	import { onMount, onDestroy } from 'svelte';
+	import IonPage from '$ionic/svelte/components/IonPage.svelte'
+	import { currentUser } from '$services/user.store'
+	import { onMount, onDestroy } from 'svelte'
 	import { addOutline } from 'ionicons/icons'
 	import SupabaseDataService from '$services/supabase.data.service'
 	import { goto } from '@roxi/routify'
@@ -10,38 +10,32 @@
 	const supabaseDataService = SupabaseDataService.getInstance()
 
 	let settings = {
-				daily_budget: 0,
-				target_weight: 0,
-				water_target: 8,
-			}	
-	let days: any[]; // = cache || []
-	let recordset;
-	onMount(async () => {
-	})
-	onDestroy(() => {
-		
-	})
+		daily_budget: 0,
+		target_weight: 0,
+		water_target: 8,
+	}
+	let days: any[] // = cache || []
+	let recordset
+	onMount(async () => {})
+	onDestroy(() => {})
 	const gotoDay = (id: string) => {
-		$goto(`/day/[id]`,{id})
+		$goto(`/day/[id]`, { id })
 	}
 	const ionViewDidEnter = async () => {
 		// console.log('** ionViewDidEnter')
 		if ($currentUser?.user_metadata) {
-			settings = $currentUser?.user_metadata;
+			settings = $currentUser?.user_metadata
 		} else {
 			settings = {
 				daily_budget: 0,
 				target_weight: 0,
 				water_target: 8,
-			}			
+			}
 		}
 		if (!recordset) {
 			// console.log('loading subscription here, user', $currentUser)
 
-			recordset = 
-			await supabaseDataService
-			.getDataSubscription('days')
-			.subscribe((recordset) => {
+			recordset = await supabaseDataService.getDataSubscription('days').subscribe((recordset) => {
 				if (recordset && recordset.length > 0) {
 					days = recordset
 				} else {
@@ -49,59 +43,63 @@
 				}
 				// console.log('*** days', days);
 			})
-
 		} else {
 			// console.log('*** attempted to re-assign recordset subscription', days);
 		}
-
 	}
 	const ionViewWillLeave = () => {
 		if (recordset) recordset.unsubscribe()
-		days = [];
+		days = []
 	}
 </script>
-<IonPage {ionViewDidEnter} {ionViewWillLeave}>
-<ion-header translucent="true">
-	<ion-toolbar>
-		<ion-buttons slot="start">
-			<ion-menu-button />
-		</ion-buttons>
-		<ion-title>Calendar</ion-title>
-		<ion-buttons slot="end">
-			<ion-button on:click={() => gotoDay('new')}>
-				<ion-icon slot="icon-only" icon={addOutline} />
-			</ion-button>				
-		</ion-buttons>
-	</ion-toolbar>
-</ion-header>
 
-<ion-content class="ion-padding">
-	<ion-list>
-		{#if days && days.length }
-			{#each days as day}
-					<ion-item on:click={() => gotoDay(day.id)} 
-						class={utils.getToday()===day.date ? 'today' : 'notToday'}>
-						{day.date}
-						<ion-note slot="end"
-						class={utils.getToday()===day.date ? 'today' : 'notToday'}>
-							{#if (utils.getToday()===day.date) && settings?.daily_budget}
+<IonPage {ionViewDidEnter} {ionViewWillLeave}>
+	<ion-header translucent="true">
+		<ion-toolbar>
+			<ion-buttons slot="start">
+				<ion-menu-button />
+			</ion-buttons>
+			<ion-title>Calendar</ion-title>
+			<ion-buttons slot="end">
+				<ion-button on:click={() => gotoDay('new')}>
+					<ion-icon slot="icon-only" icon={addOutline} />
+				</ion-button>
+			</ion-buttons>
+		</ion-toolbar>
+	</ion-header>
+
+	<ion-content class="ion-padding">
+		<ion-list>
+			{#if days && days.length}
+				{#each days as day}
+					<ion-item
+						on:click={() => gotoDay(day.id)}
+						class={utils.getToday() === day.date ? 'today' : 'notToday'}
+					>
+						{#if utils.getToday() === day.date}
+							Today
+						{:else}
+							{new Date(
+								new Date(day?.date).getTime() + new Date(day?.date).getTimezoneOffset() * 60000
+							).toDateString()}
+						{/if}
+						<ion-note slot="end" class={utils.getToday() === day.date ? 'today' : 'notToday'}>
+							{#if utils.getToday() === day.date && settings?.daily_budget}
 								<b>[{settings?.daily_budget - (day?.food_total || 0).toFixed(2)}]&nbsp;&nbsp;</b>
 							{/if}
 							{(day?.food_total || 0).toFixed(2)}
 						</ion-note>
-				</ion-item>
-			{/each}
-		{:else}
-			<ion-item>
-				No days found
-			</ion-item>
-		{/if}
-	</ion-list>
-	<!-- currentUser: {$currentUser?.id}<br/>
+					</ion-item>
+				{/each}
+			{:else}
+				<ion-item> No days found </ion-item>
+			{/if}
+		</ion-list>
+		<!-- currentUser: {$currentUser?.id}<br/>
 	settings: {JSON.stringify(settings)}<br/> -->
-
-</ion-content>
+	</ion-content>
 </IonPage>
+
 <style>
 	.today {
 		font-weight: bold;
