541483b6850beb0f040e98fcfab27651f1d0c37e
Mark Burggraf <markb@mantisbible.com>
2022-10-20 07:53:47 -0700

    0.2.8

---
+++ docs/assets/loadingMessage.ac06e7c5.js
@@ -0,0 +1,1 @@
+import"./index.b61679c1.js";import{l as t}from"./overlays-ab6211c7.72654d78.js";const e=async(r="Loading...")=>{const o=await t.create({message:r});return o.present(),o};export{e as l};
---
+++ docs/assets/user.store.91398a5a.js
@@ -0,0 +1,1 @@
+import{F as r}from"./index.b61679c1.js";const e=r(null);export{e as c};
