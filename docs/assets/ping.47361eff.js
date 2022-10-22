78f4d5a05f9227bc807f0ecde5007e4600993439
Mark Burggraf <markb@mantisbible.com>
2022-10-22 09:47:20 -0700

    0.3.6

---
+++ docs/assets/ping.47361eff.js
@@ -0,0 +1,1 @@
+import{S as l,i as r,s as p,c as u,b as f,a,H as s,g as i}from"./index.9491b236.js";function m(c){let t,n;return{c(){t=u(),n=f("ion-content"),n.textContent="pong",document.title="Ping"},m(e,o){a(e,t,o),a(e,n,o)},p:s,i:s,o:s,d(e){e&&i(t),e&&i(n)}}}class x extends l{constructor(t){super(),r(this,t,null,m,p,{})}}export{x as default};
---
+++ docs/assets/user.store.23cb988e.js
@@ -0,0 +1,1 @@
+import{F as r}from"./index.9491b236.js";const e=r(null);export{e as c};
