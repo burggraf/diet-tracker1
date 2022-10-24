0b73f23869c324bde307c540dfef5792c5317ad1
Mark Burggraf <markb@mantisbible.com>
2022-10-24 12:18:48 -0700

    chart start and end dates

--- src/routes/chart.svelte
+++ src/routes/chart.svelte
@@ -9,15 +9,18 @@
 	let chart_source = 'get_weights';
 	let chart_label = 'Weight';
 	let my_chart: any;
+	let start_date = '2022-10-01';
+	let end_date = '2022-10-31';
+	let dateChooserItem = 'start_date';
 	const ionViewDidEnter = async () => {
-		createChart(chart_source, '2022-10-01', '2022-10-31', chart_label)
+		createChart(chart_source, start_date, end_date, chart_label)
 		// createChart('get_calorie_totals', '2022-10-01', '2022-10-31', 'Calorie Totals')
 	}
 	const createChart = async (data_source: string, start_date: string, end_date: string, label: string) => {
 		let chartData = [];
 		let labels = [];
 		let datapoints = [];
-		const {data, error} = await supabaseDataService.getRPC(data_source,{start_date: '2022-10-01', end_date:'2022-10-31'});
+		const {data, error} = await supabaseDataService.getRPC(data_source,{start_date, end_date});
 		if (error) {
 			console.error('getRPC error', error);
 		} else {
@@ -93,9 +96,51 @@
 				chart_label = 'Calorie Totals';
 				break
 		}
-		createChart(chart_source, '2022-10-01', '2022-10-31', chart_label)
+		createChart(chart_source, start_date, end_date, chart_label)
 	}
+	const handleDate = async (event) => {
+		console.log('handleDate', event.target.value);
+		if (!event.target.value) return;
+		const theDate = new Date(event.target.value);
+		switch (dateChooserItem) {
+			case 'start_date':
+				start_date = theDate.toISOString().slice(0, 10);
+				break;
+			case 'end_date':
+				end_date = theDate.toISOString().slice(0, 10);
+				break;
+		}
+		// add hidden class from dateChooser
+		const dateChooser = document.getElementById('datepicker');
+		dateChooser.classList.add('hidden');
+		unHighlightDateButtons();
+		createChart(chart_source, start_date, end_date, chart_label)
+	}
+	const chooseDate = async (event) => {
+		console.log('chooseDate', event.target);
+		event.target.color = 'medium';
+		const dateChooser = document.getElementById('datepicker');
+		// check if dateChooser has hidden class
+		console.log('value', dateChooser.classList.toString().indexOf('hidden'));
+		if (dateChooser.classList.toString().indexOf('hidden') === -1) {
+			console.log('IT IS ALREADY HIDDEN')
+			dateChooser.classList.add('hidden');
+			unHighlightDateButtons();
+			// add hidden class from dateChooser
+		} else {
+			console.log('HIDE IT');
+			dateChooser.classList.remove('hidden');
 
+		}
+		console.log('chooseDate event.target.id', event.target.id);
+		dateChooserItem = event.target.id;
+		// remove hidden class from dateChooser
+		console.log('dateChooser', dateChooser);
+	}
+	const unHighlightDateButtons = () => {
+		(document.getElementById('start_date') as any).color = 'light';
+		(document.getElementById('end_date') as any).color = 'light';
+	}
 </script>
 
 <ion-header translucent="true">
@@ -115,7 +160,7 @@
 <IonPage {ionViewDidEnter}>
 	<ion-content class="ion-padding">
 		<div class="grid500 center">
-			Diet Tracker Charts... <br /><br />
+			&nbsp;
 		</div>
 		{#if $currentUser}
 			<div class="segment">
@@ -128,6 +173,14 @@
 					</ion-segment-button>
 				</ion-segment>
 			</div>
+			<div class="centered">
+				<ion-button id="start_date" size="small" color="light" on:click={chooseDate}>{start_date}</ion-button>
+				&nbsp;&nbsp;to&nbsp;&nbsp;
+				<ion-button id="end_date" size="small" color="light" on:click={chooseDate}>{end_date}</ion-button>
+			</div>
+			<div class="centered">
+				<ion-datetime id="datepicker" class="hidden" on:click={handleDate}></ion-datetime>
+			</div>	
 			<canvas id="myChart" width="400" height="400" />	
 		{:else}
 			<div class="grid500 center">not logged in</div>
@@ -146,5 +199,19 @@
 	}
 	.grid500 {
 		max-width: 500px;
+	}
+	.today {
+		font-weight: bold;
+	}
+	.notToday {
+		font-weight: normal;
+	}
+	.centered {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.hidden {
+		display: none;
 	}
 </style>
