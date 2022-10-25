bb1d4918e7ed38db8c2c4d41fca360b82ce27850
Mark Burggraf <markb@mantisbible.com>
2022-10-25 06:04:27 -0700

    0.4.5

---
+++ docs/assets/datadeletion.b055c1ba.js
@@ -0,0 +1,4 @@
+import{S as i,i as s,s as c,b as d,a as l,H as e,g as r}from"./index.31ee2d52.js";function u(o){let t;return{c(){t=d("ion-content"),t.innerHTML=`<div class="ion-padding"><div class="ion-padding" style="max-width: 375px;"><h2>Data Deletion Instructions</h2>
+Click the button below to delete your account and all associated data. This action cannot be undone.
+<br/><br/> 
+<ion-button color="danger" expand="block">Delete Account</ion-button></div></div>`},m(n,a){l(n,t,a)},p:e,i:e,o:e,d(n){n&&r(t)}}}class b extends i{constructor(t){super(),s(this,t,null,u,c,{})}}export{b as default};
---
+++ docs/assets/user.store.8187fdff.js
@@ -0,0 +1,1 @@
+import{F as r}from"./index.31ee2d52.js";const e=r(null);export{e as c};
